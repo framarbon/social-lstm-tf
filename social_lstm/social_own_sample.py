467637c8cbfd39429391e0a6dab9efca7419596f
Fran <F.Bonilla@mobilewatermanagement.com>
2017-09-10 17:30:26 +0200

    MySampler class added

--- social_lstm/social_own_sample.py
+++ social_lstm/social_own_sample.py
@@ -15,39 +15,41 @@ from social_model import SocialModel
 from grid import getSequenceGridMask
 
 
-def ownsample(obs_traj, pred_length=4, save_directory='save/1/'):
-
+class MySampler:
+    def __init__(self, save_directory='save/1/'):
     # Define the path for the config file for saved args
-    with open(os.path.join(save_directory, 'social_config.pkl'), 'rb') as f:
-        saved_args = pickle.load(f)
+        with open(os.path.join(save_directory, 'social_config.pkl'), 'rb') as f:
+            self.saved_args = pickle.load(f)
 
-    # TODO: initialize TF graph in python ros node
-    tf.reset_default_graph()
+        # TODO: initialize TF graph in python ros node
+        tf.reset_default_graph()
 
-    # Create a SocialModel object with the saved_args and infer set to true
-    model = SocialModel(saved_args, True)
-    # Initialize a TensorFlow session
-    sess = tf.InteractiveSession()
-    # Initialize a saver
-    saver = tf.train.Saver()
+        # Create a SocialModel object with the saved_args and infer set to true
+        self.model = SocialModel(self.saved_args, True)
+        # Initialize a TensorFlow session
+        self.sess = tf.InteractiveSession()
+        # Initialize a saver
+        self.saver = tf.train.Saver()
 
-    # Get the checkpoint state for the model
-    ckpt = tf.train.get_checkpoint_state('save/1')
+        # Get the checkpoint state for the model
+        ckpt = tf.train.get_checkpoint_state('save/1')
 
 
-    # Restore the model at the checkpoint
-    print ('loading model: ', ckpt.model_checkpoint_path)
-    saver.restore(sess, ckpt.model_checkpoint_path)
-    # print ('loading model: ', ckpt.all_model_checkpoint_paths[40])
-    # saver.restore(sess, ckpt.all_model_checkpoint_paths[40])
+        # Restore the model at the checkpoint
+        print ('loading model: ', ckpt.model_checkpoint_path)
+        self.saver.restore(self.sess, ckpt.model_checkpoint_path)
+        # print ('loading model: ', ckpt.all_model_checkpoint_paths[40])
+        # saver.restore(sess, ckpt.all_model_checkpoint_paths[40])
 
-    dimensions = [720, 576]
-    obs_grid = getSequenceGridMask(obs_traj, [720, 576], saved_args.neighborhood_size, saved_args.grid_size)
+    def mysample(self, obs_traj, pred_length=4,):
 
-    print "********************** SAMPLING A NEW TRAJECTORY******************************"
-    complete_traj = model.sample_nocost(sess, obs_traj, obs_grid, dimensions, pred_length)
+        dimensions = [720, 576]
+        obs_grid = getSequenceGridMask(obs_traj, [720, 576], self.saved_args.neighborhood_size, self.saved_args.grid_size)
 
-    print "Mean error of the model on this scenario is unknown",
+        print "********************** SAMPLING A NEW TRAJECTORY******************************"
+        complete_traj = self.model.sample_nocost(self.sess, obs_traj, obs_grid, dimensions, pred_length)
 
-    return complete_traj
+        print "Mean error of the model on this scenario is unknown",
+
+        return complete_traj
 
