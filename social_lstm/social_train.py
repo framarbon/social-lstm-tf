529ec38661c066727dba23a0c1f1e9d4771e0697
Fran <F.Bonilla@mobilewatermanagement.com>
2017-12-12 14:09:56 +0100

    Bug dataset dimensions & visualization

--- social_lstm/social_train.py
+++ social_lstm/social_train.py
@@ -166,10 +166,8 @@ def train(args):
 
                     if d_batch == 0 and datasets[0] == 0:
                         dataset_data = [640, 480]
-                    elif d_batch > 4:
-                        dimensions = [50, 50]
-                        print "Dimensions"
-                        print dimensions
+                    elif datasets[0] > 4:
+                        dataset_data = [50, 50]
                     else:
                         dataset_data = [720, 576]
 
@@ -241,10 +239,8 @@ def train(args):
 
                     if d_batch == 0 and datasets[0] == 0:
                         dataset_data = [640, 480]
-                    elif d_batch > 4:
-                        dimensions = [50, 50]
-                        print "Dimensions"
-                        print dimensions
+                    elif datasets[0] > 4:
+                        dataset_data = [50, 50]
                     else:
                         dataset_data = [720, 576]
 
